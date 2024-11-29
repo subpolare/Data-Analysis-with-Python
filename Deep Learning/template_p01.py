0c1ed8c6db1bdcbf70a8a4971a381fa7da1f744a
Vladislav A. Nachatoi <135843012+subpolare@users.noreply.github.com>
2024-11-29 19:30:47 +0300

    Update template_p01.py

--- Deep Learning/template_p01.py
+++ Deep Learning/template_p01.py
@@ -18,11 +18,13 @@ def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult
     decoder_hidden_state: np.array of shape (n_features_dec, 1)
     encoder_hidden_states: np.array of shape (n_features_enc, n_states)
     W_mult: np.array of shape (n_features_dec, n_features_enc)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+    attention_scores = np.dot(np.dot(decoder_hidden_state.T, W_mult), encoder_hidden_states)
+    weights_vector = softmax(attention_scores)
+    attention_vector = weights_vector.dot(encoder_hidden_states.T).T
     
     return attention_vector
 
@@ -33,10 +35,19 @@ def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add
     v_add: np.array of shape (n_features_int, 1)
     W_add_enc: np.array of shape (n_features_int, n_features_enc)
     W_add_dec: np.array of shape (n_features_int, n_features_dec)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+    encoder_transformed = np.dot(W_add_enc, encoder_hidden_states)
+    decoder_transformed = np.dot(W_add_dec, decoder_hidden_state)
+    
+    combined = encoder_transformed + decoder_transformed
+    combined_tanh = np.tanh(combined)
+    
+    attention_scores = np.dot(v_add.T, combined_tanh)
+    weights_vector = softmax(attention_scores)
+    
+    attention_vector = weights_vector.dot(encoder_hidden_states.T).T
     
     return attention_vector
